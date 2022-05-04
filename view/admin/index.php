01b71e0a1df56a58c6d874f3b0fc17321dced91a
Adi Adnyana <adiadnyana22@gmail.com>
2022-05-05 00:40:44 +0700

    Update Dashboard

--- view/admin/index.php
+++ view/admin/index.php
@@ -3,6 +3,37 @@
 ?>
 
     <h1>Ini Dashboard</h1>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="dashboard-card">
+                    <span>Total Event</span>
+                    <h2>10</h2>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="dashboard-card">
+                    <span>Total Event</span>
+                    <h2>10</h2>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="dashboard-card">
+                    <span>Total Event</span>
+                    <h2>10</h2>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="dashboard-card">
+                    <span>Total Event</span>
+                    <h2>10</h2>
+                </div>
+            </div>
+            <div class="col-12 mt-5">
+                <canvas id="myChart" width="100%" height="50"></canvas>
+            </div>
+        </div>
+    </div>
     <!-- Datatables -->
     
 <?php
