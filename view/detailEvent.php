d83149b41449e620fbabdb9d82cba4af5646fed8
Adi Adnyana <adiadnyana22@gmail.com>
2022-05-06 21:59:01 +0700

    Update Hover

---
+++ view/detailEvent.php
@@ -0,0 +1,125 @@
+<?php
+    include '../koneksi.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Name - Binus Malang Sustainable Campus</title>
+    <link rel="icon" href="../assets/images/LogoIcon.png">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" type="text/css" rel="stylesheet" /> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <!-- <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" /> -->
+    <link rel="stylesheet" href="../assets/css/styleUser.css">
+    <script src="https://kit.fontawesome.com/f0f2d9386c.js" crossorigin="anonymous"></script>
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="wrapper">
+        <!-- Header -->
+        <?php
+            include 'layout/header.php';
+        ?>
+        <div class="background">
+            <section class="bg-eventDetailInfo">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <img src="../assets/images/event.png" alt="" class="w-100">
+                        </div>
+                        <div class="offset-lg-1 col-lg-6">
+                            <div class="flex-center">
+                                <div class="eventDetailText">
+                                    <h1>Event Menanam Pohon Tahunan</h1>
+                                    <div class="row mb-2 mt-4">
+                                        <div class="col-5">
+                                            <span>Kategori</span>
+                                        </div>
+                                        <div class="col-7">
+                                            <b>Sosial</b>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-2">
+                                        <div class="col-5">
+                                            <span>Tempat</span>
+                                        </div>
+                                        <div class="col-7">
+                                            <b>Lobby Binus@Malang</b>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-2">
+                                        <div class="col-5">
+                                            <span>Tanggal Event</span>
+                                        </div>
+                                        <div class="col-7">
+                                            <b>24/11/2001</b>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-5">
+                                            <span>Tanggal Post</span>
+                                        </div>
+                                        <div class="col-7">
+                                            <b>24/11/2022</b>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="bg-eventDetailDesc">
+                <div class="container">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem incidunt magni, id possimus minima voluptatum nisi quas reiciendis. Accusantium quisquam consectetur autem itaque laborum iusto aliquam nostrum fugiat, veritatis placeat dicta ut nam consequatur ipsam tempora. Quam nostrum eum veniam veritatis cupiditate quia laboriosam nemo, sequi ut fugiat quisquam dignissimos incidunt? Commodi vero, dicta itaque autem animi ullam odio repudiandae incidunt velit veritatis, alias reprehenderit aperiam maxime sit amet, quod eum corrupti! Odit qui tempora dolorem, totam delectus necessitatibus debitis nisi consequatur explicabo aliquid quae molestiae saepe laboriosam reiciendis eaque nam maxime voluptas exercitationem accusantium rem rerum aliquam iste at. Eligendi aspernatur consequatur, aut, iure ut dolorem molestiae odio unde nobis sint repudiandae maxime quisquam ad, et accusamus nihil beatae? Iste explicabo dignissimos enim? Eligendi voluptate praesentium facere excepturi cum placeat eos blanditiis adipisci non tempore? Doloribus iure assumenda pariatur nemo deserunt asperiores quia enim voluptate? Beatae, et! Repellat sequi officiis et maiores totam numquam nemo est cum quos libero rerum commodi quod ut adipisci ullam voluptatum, quo voluptatem impedit illo suscipit molestiae eaque reiciendis mollitia odio. Ut cum ipsam, nulla dolorum laborum eum hic ducimus dignissimos sint quae voluptate labore quibusdam nihil quidem. Quam pariatur eveniet culpa omnis autem.
+                        <br>
+                        <br>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis delectus ex est nostrum molestias voluptate sequi blanditiis accusamus iusto? Ipsum dolores commodi illo quis nam nostrum quibusdam. Incidunt debitis cumque eligendi libero soluta blanditiis voluptatibus, eum, rerum perferendis sunt temporibus nemo iusto exercitationem maxime earum, rem doloribus neque eos nisi.
+                        <br>
+                        <br>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate officiis sint fugiat optio quae ipsum, dolore voluptatem earum soluta? Distinctio nobis ab quae cum amet repellat quo adipisci ipsa mollitia alias, pariatur iusto enim recusandae in doloribus assumenda temporibus labore ullam totam sunt eligendi, nisi maxime quam tenetur. Quod, quisquam accusamus delectus veritatis, dolorum provident quia iusto sit, exercitationem sed perferendis earum nostrum dicta dolor. Iste rerum quo dolores nulla blanditiis quod provident praesentium dolore a nam. Minus dolore magnam velit possimus maxime distinctio mollitia nostrum accusantium eos explicabo atque incidunt quasi quaerat, iusto vel, eveniet doloremque eligendi fuga necessitatibus!
+                        <br>
+                        <br>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla porro vero harum? Veniam illo ut ea error maiores, tempore consequatur voluptate ipsa culpa mollitia pariatur.
+                    </p>
+                </div>
+            </section>
+        </div>
+        <!-- Footer -->
+        <?php
+            include 'layout/footer.php';
+        ?>
+    </div>
+    
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> -->
+    <script>
+        document.querySelector(".mobile-overlay .close").addEventListener("click", () => {
+            document.querySelector(".mobile-overlay").style.width = 0;
+        })
+
+        document.querySelector(".bars").addEventListener("click", () => {
+            document.querySelector(".mobile-overlay").style.width = "100%";
+        })
+
+        window.addEventListener("scroll", () => {
+            if(window.scrollY <= 50){
+                document.querySelector("header").classList.remove("header-background");
+            } else {
+                document.querySelector("header").classList.add("header-background");
+            }
+        })
+    </script>
+</body>
+</html>
